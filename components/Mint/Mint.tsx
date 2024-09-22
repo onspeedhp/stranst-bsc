6aaa37f3efe37c2881bae21e9aa53d9fec7f1661
Chung Tu <tuthanhtrung2000@gmail.com>
2024-09-22 23:11:13 +0700

    feat: state mint btn

--- components/Mint/Mint.tsx
+++ components/Mint/Mint.tsx
@@ -1,18 +1,53 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import { StarBuyBtn } from '../Icon';
 import styles from './mint.module.css';
 import clsx from 'clsx';
 
 export default function Mint() {
+  const [outOfStock] = useState(false);
+  const [buyed] = useState(false);
   return (
-    <div className={clsx(styles['glow-btn'])}>
-      <div className={styles['btn-glow']} />
-      <div className={clsx('flex items-center gap-2', styles['btn'])}>
-        <StarBuyBtn />
-        <p className="text-[18px] leading-7 font-semibold text-white">
-          Buy Now
+    <>
+      {buyed ? (
+        <p className="text-[18px] leading-7 font-semibold text-slate-50">
+          Congratulation! You’re one of the Strant VIP Pass holder.
         </p>
-      </div>
-    </div>
+      ) : (
+        <>
+          {outOfStock ? (
+            <div
+              className={clsx(
+                styles['out-of-stock'],
+                'opacity-50 pointer-events-none'
+              )}
+            >
+              <div
+                className="w-full h-full rounded-[35px] flex items-center justify-center gap-2"
+                style={{
+                  background:
+                    'linear-gradient(90deg, #9747FF 0%, #EA1187 100%)',
+                }}
+              >
+                <StarBuyBtn />
+                <p className="text-[18px] leading-7 font-semibold text-white">
+                  Out Of Stock
+                </p>
+              </div>
+            </div>
+          ) : (
+            <div className={clsx(styles['glow-btn'])}>
+              <div className={styles['btn-glow']} />
+              <div className={clsx('flex items-center gap-2', styles['btn'])}>
+                <StarBuyBtn />
+                <p className="text-[18px] leading-7 font-semibold text-white">
+                  Buy Now
+                </p>
+              </div>
+            </div>
+          )}
+        </>
+      )}
+    </>
   );
 }
